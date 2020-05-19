1ccfe77bc4cee17802faf0ca5a5d81d0030f5ec6
frederic jonquieres <fjonquieres83@gmail.com>
2020-05-19 11:51:56 +0100

    RST-2598 Text area

--- test/dummy/app/models/kitchen_sink.rb
+++ test/dummy/app/models/kitchen_sink.rb
@@ -2,8 +2,10 @@ class KitchenSink
   include ActiveModel::Model
   include ActiveModel::Attributes
   attribute :text_field_value, :string
+  attribute :text_area_value, :string
 
   def add_example_errors
     errors.add :text_field_value, 'Text field error'
+    errors.add :text_area_value, 'Text area error'
   end
 end
