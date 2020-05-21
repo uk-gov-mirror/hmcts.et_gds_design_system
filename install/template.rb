69394f0035faf9832771fe8786fc8aa6e0ff273a
Gary Taylor <gary.taylor@hismessages.com>
2020-05-21 07:16:32 +0100

    Fixed bug in generator

--- install/template.rb
+++ install/template.rb
@@ -1,6 +1,6 @@
 if EtGdsDesignSystem::VERSION =~ /^[0-9]+\.[0-9]+\.[0-9]+$/
   say "Installing all JavaScript dependencies [#{EtGdsDesignSystem::VERSION}]"
-  run "yarn add https://github.com/hmcts/et_gds_design_system.git#{EtGdsDesignSystem::VERSION}"
+  run "yarn add https://github.com/hmcts/et_gds_design_system.git##{EtGdsDesignSystem::VERSION}"
 else
   say "Installing all JavaScript dependencies [from prerelease]"
   run "yarn add https://github.com/hmcts/et_gds_design_system.git"
