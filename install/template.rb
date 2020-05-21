2caed15b77214b9aac5e121cc9715e2767efc8f3
Gary Taylor <gary.taylor@hismessages.com>
2020-05-21 07:27:49 +0100

    Fixed bug in generator

--- install/template.rb
+++ install/template.rb
@@ -6,4 +6,15 @@ else
   run "yarn add https://github.com/hmcts/et_gds_design_system.git"
 end
 
+if File.exists?("app/javascript/packs/application.js")
+  append_to_file "app/javascript/packs/application.js" do
+    <<-EOS
+    \n
+    import {EtGdsDesignSystem } from "et_gds_design_system"
+    import "et_gds_design_system/stylesheet"
+    EtGdsDesignSystem.initAll();
+    EOS
+  end
+end
+
 say "EtGdsDesignSystem successfully installed 🎉 🍰", :green
