1ede316796978ed88e0498f9833a70261df1cd49
frederic jonquieres <fjonquieres83@gmail.com>
2020-05-26 13:42:37 +0100

    RST-2595 Collection Radio Buttons

--- test/dummy/app/models/kitchen_sink.rb
+++ test/dummy/app/models/kitchen_sink.rb
@@ -4,10 +4,12 @@ class KitchenSink
   attribute :text_field_value, :string
   attribute :text_area_value, :string
   attribute :date_field_value, :date
+  attribute :collection_radio_buttons_value, :boolean
 
   def add_example_errors
     errors.add :text_field_value, 'Text field error'
     errors.add :text_area_value, 'Text area error'
     errors.add :date_field_value, 'Date field error'
+    errors.add :collection_radio_buttons_value, 'Collection radio buttons error'
   end
 end
