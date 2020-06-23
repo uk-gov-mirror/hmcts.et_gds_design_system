33e700a971bfe46241235099ab3bdc1ccc4f2fcd
frederic jonquieres <fjonquieres83@gmail.com>
2020-06-23 15:25:14 +0100

    RST-2601 GOVUK Error summary Test

--- spec/features/govuk_error_summary_spec.rb
+++ spec/features/govuk_error_summary_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe 'govuk_fieldset', type: :feature, js: true do
     test_page.with_errors
     expect(test_page).to have_govuk_error_summary
   end
+
+  it 'has correct errors' do
+    aggregate_failures 'validating content' do
+      test_page.load
+      test_page.with_errors
+      expect(test_page).to have_text('Test error summary')
+      expect(test_page).to have_text('Text field error')
+      expect(test_page).to have_text('Text area error')
+      expect(test_page).to have_text('Date field error')
+      expect(test_page).to have_text('Collection radio buttons error')
+      expect(test_page).to have_text('Phone field error')
+      expect(test_page).to have_text('Email field error')
+      expect(test_page).to have_text('Collection check boxes error')
+    end
+  end
 end
