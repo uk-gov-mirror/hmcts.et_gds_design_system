edf6480229bc50aea85ffe318ffac5ea8e8a3dae
Gary Taylor <gary.taylor@hismessages.com>
2021-02-06 11:06:38 +0000

    initAll was not initialising turbolinks

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const GOVUKFrontend = require("govuk-frontend/govuk/all")
 require.context('govuk-frontend/govuk/assets/images', true)
+require('turbolinks').start();
 import './stylesheet'
-const EtGdsDesignSystem = {};
+import Components from './app/javascript/components'
+const EtGdsDesignSystem = {
+    Components: Components
+};
 EtGdsDesignSystem.initAll = () => {
     console.log("EdGdsDesignSystem.initAll was called");
     const onPageLoad = () => {
         document.body.className = ((document.body.className) ? document.body.className + ' js-enabled' : 'js-enabled');
 
-        GOVUKFrontend.initAll()
+        GOVUKFrontend.initAll();
+        EtGdsDesignSystem.Components.Reveal.init();
     }
     document.addEventListener("turbolinks:load", onPageLoad);
 };
