341ff4aa62a7294dcd45ba20ad06e829fc386c52
Gary Taylor <gary.taylor@hismessages.com>
2022-04-22 18:04:42 +0100

    Works with rails 7

--- config/initializers/type.rb
+++ config/initializers/type.rb
@@ -1,4 +1,4 @@
-ActiveSupport.on_load(:after_initialize) do
+Rails.application.config.to_prepare do
   ActiveRecord::Type.register(:date_without_day, EtGdsDesignSystem::DateWithoutDayType)
   ActiveRecord::Type.register(:gds_azure_file, EtGdsDesignSystem::AzureFile)
 end
