816600938f2024ff4ec74a62857977c7d48c3f5b
Gary Taylor <gary.taylor@hismessages.com>
2022-10-14 07:30:18 +0100

    Switched file upload mechanism to go via front end app through to the api

--- lib/et_gds_design_system/api_proxy.rb
+++ lib/et_gds_design_system/api_proxy.rb
@@ -3,12 +3,23 @@ require 'rack-proxy'
 module EtGdsDesignSystem
   class ApiProxy < Rack::Proxy
     def rewrite_env(env)
-      et_api_uri = URI.parse(Rails.application.config.et_gds_design_system.api_url)
-      env.merge 'HTTP_HOST' => et_api_uri.host,
-                'REQUEST_URI' => "#{et_api_uri}/build_blob",
-                'REQUEST_PATH' => "/api/v2/build_blob",
-                'SCRIPT_NAME' => "/api/v2/build_blob",
+      env.merge 'REQUEST_URI' => correct_host(env['REQUEST_URI']),
+                'HTTP_HOST' => "#{et_api_uri.host}:#{et_api_uri.port}",
                 'PATH_INFO' => ''
+    end
+
+    private
+
+    def correct_host(url)
+      uri = URI.parse(url)
+      uri.scheme = et_api_uri.scheme
+      uri.host = et_api_uri.host
+      uri.port = et_api_uri.port
+      uri.to_s
+    end
+
+    def et_api_uri
+      @et_api_uri ||= URI.parse(Rails.application.config.et_gds_design_system.api_url)
     end
   end
 end
